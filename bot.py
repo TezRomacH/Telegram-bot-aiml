fd0b3c05843a12d9daef89617e576e683282f5ec
TezRomacH <tez.romach@gmail.com>
2018-01-08 01:46:28 +0300

    add TRANSLATE pattern

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import config
+import constants
 import random
 
 import aiml
@@ -23,32 +24,35 @@ def log_messages(text, from_user, bot_response, type="message"):
 
 @bot.message_handler(commands=["start"])
 def handle_start(message):
-    bot.send_message(message.chat.id, config.title_ru)
+    bot.send_message(message.chat.id, constants.title_ru)
     log_messages(
         "/start",
         message.chat.first_name + " " + message.chat.last_name,
-        config.title_ru,
+        constants.title_ru,
         type="command"
     )
 
 
 @bot.message_handler(commands=["help"])
 def handle_help(message):
-    bot.send_message(message.chat.id, config.help_ru)
+    bot.send_message(message.chat.id, constants.help_ru)
     log_messages(
         "/help",
         message.chat.first_name + " " + message.chat.last_name,
-        config.title_ru,
+        constants.title_ru,
         type="command"
     )
 
 
-def translate_empty(dest):
-    text = random.choice(config.random_russian_words)
-    response_text = config.empty_translate_text_message.format(
-        text,
-        translator.translate(text, dest=dest).text
-    )
+def translate_text(text, dest):
+    if text == "":
+        text = random.choice(constants.random_russian_words)
+        response_text = constants.empty_translate_text_message.format(
+            text,
+            translator.translate(text, dest=dest).text
+        )
+    else:
+        response_text = translator.translate(text, dest=dest).text
     return response_text
 
 
@@ -57,19 +61,13 @@ def handle_translate(message):
     command_len = len("/translate") + 1
 
     text = message.text[command_len:]
-    stored_dest_lang = kernel.getPredicate("translate_dest_lang", message.chat.id)
-    stored_dest_lang = stored_dest_lang if stored_dest_lang in config.languages else "en"
-    if text == "":
-        response_text = translate_empty(stored_dest_lang)
-    elif text[:2] in config.languages:
-        dest = text[:2]
+    dest_lang = kernel.getPredicate("translate_dest_lang", message.chat.id)
+    dest_lang = dest_lang if dest_lang in constants.languages else "en"
+
+    if text[:2] in constants.languages:
+        dest_lang = text[:2]
         text = text[3:]
-        if text == "":
-            response_text = translate_empty(dest)
-        else:
-            response_text = translator.translate(text, dest=dest).text
-    else:
-        response_text = translator.translate(text, dest=stored_dest_lang).text
+    response_text = translate_text(text, dest=dest_lang)
 
     log_messages(
         message.text,
@@ -85,6 +83,12 @@ def handle_translate(message):
 def response(message):
     kernel.setPredicate("name", message.chat.first_name + " " + message.chat.last_name, message.chat.id)
     response_text = kernel.respond(message.text, message.chat.id)
+    if message.text.lower().startswith(constants.translate_key_ru):
+        text = kernel.getPredicate("translate_text", message.chat.id)
+        dest_lang = kernel.getPredicate("translate_dest_lang", message.chat.id)
+        dest_lang = dest_lang if dest_lang in constants.languages else "en"
+        response_text += " " + translate_text(text, dest=dest_lang)
+
     log_messages(
         message.text,
         message.chat.first_name + " " + message.chat.last_name,
